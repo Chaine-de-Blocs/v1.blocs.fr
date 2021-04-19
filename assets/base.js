c7d6727d6cd3c4752bd38979135eed2143f5f1fa
Slals <jonathan@blocs.fr>
2021-04-19 03:07:49 +0200

    Updates base.js

--- assets/base.js
+++ assets/base.js
@@ -1,27 +1,34 @@
-var storageKey = 'blocs-data-theme';
-(function() {
-    document.addEventListener("DOMContentLoaded", function() { 
-        let currentTheme = localStorage.getItem(storageKey);
-        const themeSwitchElt = document.querySelector('#theme-switch');
-        if (themeSwitchElt) {
-            activateTheme(currentTheme, themeSwitchElt);
-            
-            themeSwitchElt.addEventListener('click', function () {
-                switch (currentTheme) {
-                    case 'dark':
-                        currentTheme = 'light';
-                        break;
-                    default:
-                        currentTheme = 'dark';
-                }
-                localStorage.setItem(storageKey, currentTheme);
-                activateTheme(currentTheme, themeSwitchElt)
-            });
-        }
-    });
-})();
+const storageKey = 'blocs-data-theme';
 
-function activateTheme(theme, switchElt) {
+
+document.addEventListener("DOMContentLoaded", function() { 
+    const userPrefersDark = window.matchMedia
+        && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    let currentTheme = window.localStorage.getItem(storageKey);
+    if (!currentTheme) {
+        currentTheme = userPrefersDark ? 'dark' : 'light';
+    }
+    
+    const themeSwitchElt = document.querySelector('#theme-switch');
+    if (themeSwitchElt) {
+        activateTheme(currentTheme, themeSwitchElt);
+        
+        themeSwitchElt.addEventListener('click', function () {
+            switch (currentTheme) {
+                case 'dark':
+                    currentTheme = 'light';
+                    break;
+                default:
+                    currentTheme = 'dark';
+            }
+            window.localStorage.setItem(storageKey, currentTheme);
+            activateTheme(currentTheme, themeSwitchElt)
+        });
+    }
+});
+
+const activateTheme = (theme, switchElt) => {
     switch (theme) {
         case 'light':
             switchElt.innerHTML = 'Activer le mode sombre';
